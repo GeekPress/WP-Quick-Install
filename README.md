fb116bbc3b456a1132dff7e49736efb2e52b7cf2
GeekPress <contact@geekpress.fr>
2012-08-26 22:39:32 +0200

    optimisation du code

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-WP-Quick-Install 1.0
+WP-Quick-Install 1.1
 ================
 
 WP Quick Install est un script permettant d'installer WordPress en seul clic (téléchargement, décompression, installation de plugins, création base de données, etc...). 
--- wp-quick-install/index.php
+++ wp-quick-install/index.php
@@ -1,232 +1,183 @@
 <?php
 set_time_limit(0);
 
-/*-----------------------------------------------------------------------------------*/
-/*	On construit le tableau de configuration
-/*-----------------------------------------------------------------------------------*/
-
-// On récupère les données du fichier data.txt
-if( file_exists( 'data.txt' ) ) {
-	
-	$data = array();
-	
-	foreach ( file( 'data.txt' ) as $line ) {
-		$line = explode( ":", $line );
-		$data[trim($line[0])] = trim( $line[1] );
-	}
-	
-	// On transforme les éléments du tableau en variable
-	extract($data);
-	
-	// On ajoute  ../ au directory
-	$directory = !empty($directory) ? '../' .$directory . '/' : '../' ;
-	
-	// On construit un tableau avec les configurations de la BDD
-	$db_config = array();
-	$tmp_db_config = explode( ";", $db );
-	
-	foreach ( $tmp_db_config as $db_line ) {
-		$db_lines = explode( "=", $db_line );
-		$db_config[trim($db_lines[0])] = trim( $db_lines[1] );
-	} // foreach
-	
-	// On construit un tableau avec les configurations de l'admnistrateur
-	$admin_config = array();
-	$tmp_admin_config = explode( ";", $admin );
-	
-	foreach ( $tmp_admin_config as $admin_line ) {
-		$admin_lines = explode( "=", $admin_line );
-		$admin_config[trim($admin_lines[0])] = trim( $admin_lines[1] );
-	} // foreach
-}
+// On ajoute  ../ au directory
+$directory = !empty( $_POST['directory'] ) ? '../' . $_POST['directory'] . '/' : '../';
 
 // Allez, maintenant on commence les choses sérieuses haha =D
 if( isset( $_GET['action'] ) ) {
-	
+
 	switch( $_GET['action'] ) {
-		
+
 		case "check_db_connection" :
-			
+
 			/*-----------------------------------------------------------------------------------*/
 			/*	On check si on arrive à se connecter à la base de données
 			/*-----------------------------------------------------------------------------------*/
-			
+
 			try {
 			   $db = new PDO('mysql:host='. $_POST['dbhost'] .';dbname=' . $_POST['dbname'] , $_POST['uname'], $_POST['pwd'] );
 			} // try
 			catch(Exception $e) {
 				echo "error etablishing connection";
 			} // catch
-			
+
 			break;
-		
-		case "create_data" :
-			
-			/*-----------------------------------------------------------------------------------*/
-			/*	On crée le fichier data.txt qui va contenir l'ensemble de la configuration
-			/*-----------------------------------------------------------------------------------*/
-				
-			// On définit le contenu du fichier data.txt
-			$data_file = 'directory: ' . $_POST[ 'directory' ] . "\r\n";
-			$data_file .= 'weblog_title: ' . $_POST[ 'weblog_title' ] . "\r\n";
-			$data_file .= 'db: prefix=' . $_POST[ 'prefix' ] . '; dbname=' . $_POST[ 'dbname' ] . '; dbhost=' . $_POST[ 'dbhost' ] . '; uname=' . $_POST[ 'uname' ] . '; pwd=' . $_POST[ 'pwd' ] . "\r\n";
-			$data_file .= 'admin: user_name= ' . $_POST[ 'user_name' ] . '; admin_password= ' . $_POST[ 'admin_password' ] . '; admin_email=' . $_POST[ 'admin_email' ] . ' ' . "\r\n";
-			$data_file .= 'seo: ' . (int)$_POST[ 'seo' ] . "\r\n";
-			$data_file .= 'plugins: ' . $_POST[ 'plugins' ];
-			
-			// On crée le fichier data.txt
-			file_put_contents( 'data.txt', $data_file);
-			
-			break;
-		
+
 		case "download_wp" :
-			
+
 			/*-----------------------------------------------------------------------------------*/
 			/*	On récupère la dernière version de WordPress et on la place à la racine
 			/*-----------------------------------------------------------------------------------*/
-			
+
 			if( !file_exists( 'wordpress.zip' ) )
-				file_put_contents( 'wordpress.zip', file_get_contents( 'http://fr.wordpress.org/latest-fr_FR.zip' ) );			
-			
+				file_put_contents( 'wordpress.zip', file_get_contents( 'http://fr.wordpress.org/latest-fr_FR.zip' ) );
+
 			break;
-		
+
 		case "unzip_wp" :
-			
+
 			/*-----------------------------------------------------------------------------------*/
 			/*	On crée le dossier du site avec les fichiers et dossiers "wordpress"
 			/*-----------------------------------------------------------------------------------*/
-			
+
 			// On check si WP n'est pas déjà installé
 			if( !file_exists( $directory . 'wp-config.php' ) ) {
-				
+
 				// Si on souhaite mettre WordPress dans un sous-dossier, on le crée
 				if( !empty( $directory ) ) {
-					mkdir( $directory );
 					
+					// On crée le dossier
+					mkdir( $directory );
+
 					// On met à jour les droits d'écriture du fichier
 					chmod( $directory , 0755);
 				}
-				
+
 				// On dézip le fichier
 				exec( 'unzip wordpress' );
-				
+
 				// On fait une copie du dossier
 				exec( 'cp -rp wordpress/* ' . $directory );
-				
+
 				// On supprime le dossier wordpress
 				exec( 'rm -rf wordpress' );
-				
+
 			}
 			break;
-		
+
 		case "install_plugins" :
-			
+
 			/*-----------------------------------------------------------------------------------*/
 			/*	On récupère les dossiers des plugins
 			/*-----------------------------------------------------------------------------------*/
-			
-			if( !empty( $plugins ) ) {
-					
-				$plugins_directory = $directory . 'wp-content/plugins/'; // Dossier qui contient les plugins			
-			
+
+			if( !empty( $_POST['plugins'] ) ) {
+				
+				$plugins = explode( ";", $_POST['plugins'] ); // On récupère la liste des plugins dans un tableau
+				$plugins_directory = $directory . 'wp-content/plugins/'; // Dossier qui contient les plugins
+
 				 // On boucle tous les plugins que l'on doit installé
-				foreach( explode( ";" , $plugins ) as $plugin ) {
-					
+				foreach( $plugins as $plugin ) {
+
 					// On check si le plugin est déjà installé
 					if( !is_dir( $plugins_directory . $plugin ) ) {
-						
+
 						// On récupère le fichier XML du plugin pour récupérer le lien vers le zip du plugin
 						$xml = @simplexml_load_file('http://api.wordpress.org/plugins/info/1.0/' . trim( $plugin ) . '.xml');
-						
+
 					    if( $xml != NULL ) {
-					    	
+
 					    	// On récupère la dernière version de WordPress et on la place à la racine
 					    	file_put_contents( $plugins_directory . md5( $plugin ) . '.zip', file_get_contents( $xml->download_link ) );
-					    	
+
 					    	// On dézip le fichier
 							exec( 'tar xvzf ' . $plugins_directory . md5( $plugin ) . '.zip -C ' . $plugins_directory .'/' );
-					    	
+
 					    	// On  supprime l'archive temporaire
 					    	unlink( $plugins_directory . md5( $plugin ) . '.zip' );
-					    
+
 					    } // if
-					}					
-				} // foreach				
+					}
+				} // foreach
 			}
-			
+
 			break;
-			
+
 			case "wp_config" :
-				
+
 				/*-----------------------------------------------------------------------------------*/
 				/*	On crée le fichier wp-config.php
-				/*-----------------------------------------------------------------------------------*/			
-				
+				/*-----------------------------------------------------------------------------------*/
+
 				// Ok trop cool, on a WP et ses plugins, maintenant on commence les choses sérieuses =-)
-				
+
 				// On récupère les lignes du fichier wp-config-sample.php sous forme de tableau
 				$config_file = file( $directory . 'wp-config-sample.php' );
-				
+
 				// Gestion des clés de sécurité
 				$secret_keys = explode( "\n", file_get_contents( 'https://api.wordpress.org/secret-key/1.1/salt/' ) );
-				
+
 				foreach ( $secret_keys as $k => $v ) {
 					$secret_keys[$k] = substr( $v, 28, 64 );
 				}
-				
+
 				// On remplace les lignes nécassaire du fichier
 				$key = 0;
 				foreach ( $config_file as &$line ) {
-					
+
 					if ( '$table_prefix  =' == substr( $line, 0, 16 ) ) {
-						$line = '$table_prefix  = \'' . addcslashes( $db_config['prefix'], "\\'" ) . "';\r\n";
+						$line = '$table_prefix  = \'' . addcslashes( $_POST[ 'prefix' ], "\\'" ) . "';\r\n";
 						continue;
 					}
-			
+
 					if ( ! preg_match( '/^define\(\'([A-Z_]+)\',([ ]+)/', $line, $match ) )
 						continue;
-			
+
 					$constant = $match[1];
 					$padding  = $match[2];
-			
+
 					switch ( $constant ) {
 						case 'WP_DEBUG'	   :
-							
+
 							// Mode Debug
 							if( (int)$_POST['debug'] == 1 )
 								$line = "define('" . $constant . "'," . $padding . "'true');\r\n";
-							
+
 							// On ajoute les constantes supplémentaires
 							if( (int)$_POST['post_revisions'] == 1 ) {
 								$line .= "\r\n\n " . "/** Désactivation des révisions d'articles */" . "\r\n";
 								$line .= "define('WP_POST_REVISIONS', false);";
 								fwrite($handle, $line);
 							}
-							
+
 							if( (int)$_POST['disallow_file_edit'] == 1 ) {
 								$line .= "\r\n\n " . "/** Désactivation de l'éditeur de thème et d'extension */" . "\r\n";
 								$line .= "define('DISALLOW_FILE_EDIT', false);";
 								fwrite($handle, $line);
 							}
-							
+
 							if( (int)$_POST['autosave_interval'] >= 1 ) {
 								$line .= "\r\n\n " . "/** Intervalle des sauvegardes automatique */" . "\r\n";
 								$line .= "define('AUTOSAVE_INTERVAL', " . (int)$_POST['autosave_interval'] . ");";
 								fwrite($handle, $line);
 							}
 							
+							$line .= "\r\n\n " . "/** On augmente la mémoire limite */" . "\r\n";
+							$line .= "define('WP_MEMORY_LIMIT', '96M')" . "\r\n";
+
 							break;
 						case 'DB_NAME'     :
-							$line = "define('" . $constant . "'," . $padding . "'" . addcslashes( $db_config['dbname'], "\\'" ) . "');\r\n";
+							$line = "define('" . $constant . "'," . $padding . "'" . addcslashes( $_POST[ 'dbname' ], "\\'" ) . "');\r\n";
 							break;
 						case 'DB_USER'     :
-							$line = "define('" . $constant . "'," . $padding . "'" . addcslashes( $db_config['uname'], "\\'" ) . "');\r\n";
+							$line = "define('" . $constant . "'," . $padding . "'" . addcslashes( $_POST['uname'], "\\'" ) . "');\r\n";
 							break;
 						case 'DB_PASSWORD' :
-							$line = "define('" . $constant . "'," . $padding . "'" . addcslashes( $db_config['pwd'], "\\'" ) . "');\r\n";
+							$line = "define('" . $constant . "'," . $padding . "'" . addcslashes( $_POST['pwd'], "\\'" ) . "');\r\n";
 							break;
 						case 'DB_HOST'     :
-							$line = "define('" . $constant . "'," . $padding . "'" . addcslashes( $db_config['dbhost'], "\\'" ) . "');\r\n";
+							$line = "define('" . $constant . "'," . $padding . "'" . addcslashes( $_POST['dbhost'], "\\'" ) . "');\r\n";
 							break;
 						case 'AUTH_KEY'         :
 						case 'SECURE_AUTH_KEY'  :
@@ -241,59 +192,47 @@ if( isset( $_GET['action'] ) ) {
 					} // switch
 				} // foreach
 				unset( $line );
-				
+
 				$handle = fopen($directory . 'wp-config.php', 'w');
 				foreach( $config_file as $line ) {
 					fwrite($handle, $line);
-				} // foreach				
+				} // foreach
 				fclose($handle);
-				
+
 				// On met à jour les droits d'écriture du fichier
 				chmod($directory . 'wp-config.php', 0666);
-				
+
 				break;
-				
+
 			case "install_wp" :
-				
+
 				/*-----------------------------------------------------------------------------------*/
 				/*	On installe la BDD de WordPress
 				/*-----------------------------------------------------------------------------------*/
-				
+
 				/** Load WordPress Bootstrap */
 				require_once( $directory . 'wp-load.php' );
-				
+
 				/** Load WordPress Administration Upgrade API */
 				require_once( $directory . 'wp-admin/includes/upgrade.php' );
-				
+
 				/** Load wpdb */
 				require_once( $directory . 'wp-includes/wp-db.php' );
-				
-				
+
+
 				// On installe WordPress
-				wp_install( $weblog_title, $admin_config['user_name'], $admin_config['admin_email'], (int)$seo, '', $admin_config['admin_password'] );
-				
+				wp_install( $_POST[ 'weblog_title' ], $_POST['user_name'], $_POST['admin_email'], (int)$_POST[ 'seo' ], '', $_POST['admin_password'] );
+
 				// On met à jour les options siteurl et home avec la bonne adresse URL
-				$url = trim( str_replace( basename(dirname(__FILE__)) . '/index.php/wp-admin/install.php?action=install_wp' , str_replace( '../', '', $directory ), 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'] ), '/' ); // On définit l'adresse à utiliser pour les options siteurl et home_url				
+				$url = trim( str_replace( basename(dirname(__FILE__)) . '/index.php/wp-admin/install.php?action=install_wp' , str_replace( '../', '', $directory ), 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'] ), '/' ); // On définit l'adresse à utiliser pour les options siteurl et home_url
 				update_option( 'siteurl', $url );
 				update_option( 'home', $url );
-				
+
 				break;
-			
-			case "delete_data" :
 				
-				/*-----------------------------------------------------------------------------------*/
-				/*	On supprime le fichier data.txt.
-				/*  NOTE : On ne supprime pas le dossier wordpress.zip si on souhaite dse resservir du script
-				/*	sans re-télécharger WordPress
-				/*-----------------------------------------------------------------------------------*/
-			
-				unlink( 'data.txt' );
-				
-				break;
 	} // switch
 } // if isset( $_GET['action'] )
 else { ?>
-
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" lang="fr">
 	<head>
@@ -312,18 +251,18 @@ else { ?>
 			<p>L'installation de WordPress s'est déroulée avec succès.</p>
 		</div>
 		<form method="post" action="">
-			
+
 			<div class="alert alert-error" style="display:none;">
 				<strong>Attention !</strong>
 				<p style="margin-bottom:0px;">Erreur de connexion à la base de données. Merci de vérifier vos identifiants.</p>
 			</div>
-						
-			<h1>Avertissement</h1>			
+
+			<h1>Avertissement</h1>
 			<p>Ce fichier doit obligatoirement se trouver dans le dossier <em>wp-quick-install</em>. Il ne doit pas être présent à la racine du projet ou de votre FTP.</p>
-			
+
 			<h1>Informations de la base de données</h1>
 			<p>Vous devez saisir ci-dessous les détails de connexion à votre base de données. Si vous ne les connaissez pas, contactez votre hébergeur.</p>
-			
+
 			<table class="form-table">
 				<tr>
 					<th scope="row"><label for="dbname">Nom de la base de données</label></th>
@@ -351,10 +290,10 @@ else { ?>
 					<td>Si vous souhaitez faire tourner plusieurs installations de WordPress sur une même base de données, modifiez ce réglage.</td>
 				</tr>
 			</table>
-			
+
 			<h1>Informations nécessaires</h1>
 			<p>Merci de fournir les informations suivantes. Ne vous inquiétez pas, vous pourrez les modifier plus tard.</p>
-			
+
 			<table class="form-table">
 				<tr>
 					<th scope="row">
@@ -396,7 +335,7 @@ else { ?>
 					<td colspan="2"><label><input type="checkbox" name="blog_public" value="1" checked='checked' /> Demander aux moteurs de recherche d&rsquo;indexer ce site.</label></td>
 				</tr>
 			</table>
-			
+
 			<h1>Informations extensions</h1>
 			<p>Vous devez saisir ci-dessous les extensions qui doivent être ajoutées pendant l'installation.</p>
 			<div class="alert alert-info">
@@ -414,10 +353,10 @@ else { ?>
 					</td>
 				</tr>
 			</table>
-			
+
 			<h1>Informations wp-config.php</h1>
 			<p>Vous devez choisir ci-dessous les constantes supplémentaires à ajouter dans le fichier <strong>wp-config.php</strong>.</p>
-			
+
 			<table class="form-table">
 				<tr>
 					<th scope="row">
@@ -448,21 +387,20 @@ else { ?>
 					</td>
 				</tr>
 			</table>
-			
 			<p class="step"><span id="submit" class="button">Installer WordPress</span></p>
 		</form>
-		
+
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
 		<script>
-		
+
 			$(document).ready(function() {
-									
-				var $response = $('#response');
-				
-				$('#submit').click( function( evt ) {
-					
+
+				var $response  = $('#response');
+
+				$('#submit').click( function() {
+
 					var errors = false;
-					
+
 					$('input.required').each(function(){
 						if( $.trim($(this).val()) == '' ) {
 							errors = true;
@@ -472,7 +410,7 @@ else { ?>
 							$(this).css("border", "1px solid #DFDFDF");
 						} // else
 					});
-					
+
 					// On check la connexion à la BDD
 					$.post('<?php echo $_SERVER['PHP_SELF'] ?>?action=check_db_connection', $('form').serialize(), function(data) {
 						if( data == "error etablishing connection" ) {
@@ -484,10 +422,16 @@ else { ?>
 							if( !errors ) {
 								$('form').fadeOut( 'fast', function(){
 									
-									// On récupère les données du formulaire
-									$.post('<?php echo $_SERVER['PHP_SELF'] ?>?action=create_data', $('form').serialize(), function(data) {
-										step1();
-									});	
+									// ETAPE 1
+									// On récupère l'archive de la dernière version de WordPress
+									$response.html("<p>Téléchargement de l'archive de WordPress en cours...</p>");
+									
+									// On montre la barre de progression
+									$('.progress').show();
+									
+									$.get('<?php echo $_SERVER['PHP_SELF'] ?>?action=download_wp', function(data) {
+										step2();
+									});
 								});
 							}
 							else {
@@ -495,42 +439,31 @@ else { ?>
 							}
 							return false;
 						} // else
-					});	
-				});
-					
-				// ETAPE 1
-				// On récupère l'archive de la dernière version de WordPress
-				function step1() {
-					$response.html("<p>Téléchargement de l'archive de WordPress en cours...</p>");
-					// On montre la barre de progression
-					$('.progress').show();
-					$.get('<?php echo $_SERVER['PHP_SELF'] ?>?action=download_wp', function(data) {
-						step2();
 					});
-				}
-				
+				});
+
 				// ETAPE 2
-				// Décompression de l'archive de WordPress					
+				// Décompression de l'archive de WordPress
 				function step2() {
 					$response.html("<p>Installation des fichiers en cours...</p>" );
 					$('.progress .bar').animate({width: "20%"});
-					$.get('<?php echo $_SERVER['PHP_SELF'] ?>?action=unzip_wp', function(data) {
+					$.post('<?php echo $_SERVER['PHP_SELF'] ?>?action=unzip_wp', $('form').serialize(), function(data) {
 						step3();
 					});
 				}
-				
+
 				// ETAPE 3
 				// Installation des plugins
-				
+
 				function step3() {
 					$response.html("<p>Installation des extensions en cours...</p>");
 					$('.progress .bar').animate({width: "40%"});
-					$.get('<?php echo $_SERVER['PHP_SELF'] ?>?action=install_plugins', function(data) {
+					$.post('<?php echo $_SERVER['PHP_SELF'] ?>?action=install_plugins', $('form').serialize(), function(data) {
 						$response.html(data);
 						step4();
 					});
 				}
-				
+
 				// ETAPE 4
 				// Création du fichier wp-config.php
 				function step4() {
@@ -540,33 +473,30 @@ else { ?>
 						step5();
 					});
 				}
-						
+
 				// ETAPE 5
 				// Création de la BDD et de l'administrateur
 				function step5() {
 					$response.html("<p>Création de la BDD et de l'administrateur en cours...</p>");
 					$('.progress .bar').animate({width: "80%"});
-					$.get('<?php echo $_SERVER['PHP_SELF'] ?>/wp-admin/install.php?action=install_wp', function(data) {
+					$.post('<?php echo $_SERVER['PHP_SELF'] ?>/wp-admin/install.php?action=install_wp', $('form').serialize(), function(data) {
 						step6();
 					});
 				}
-				
+
 				//ETAPE 6
 				// Suppression de l'archive d'origine
 				function step6() {
 					$response.html("<p>Installation terminée.</p>");
 					$('.progress .bar').animate({width: "100%"});
-					$.get('<?php echo $_SERVER['PHP_SELF'] ?>?action=delete_data', function(data) {
-						$response.delay(500).hide();
-						$('.progress').delay(500).hide();
-						$('#success').delay(500).show();
-					});
+					$response.delay(500).hide();
+					$('.progress').delay(500).hide();
+					$('#success').delay(500).show();
 				}
 			});
 		</script>
 	</body>
 </html>
-
 <?php
 }
 ?>
