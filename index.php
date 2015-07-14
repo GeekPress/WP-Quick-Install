45ba9ea15df4a0c202c5efa30850d7c8fad4e199
pravdomil <pravdomil.toman@gmail.com>
2015-07-14 02:01:12 +0200

    list all user options

--- index.php
+++ index.php
@@ -1,14 +1,27 @@
 <?php
 
-$user_config = array (
-    "auto_installer" => false,
-    "db" => array(
-        //"name" => "wordpress",
-        //"user" => "",
-        //"pwd" => "",
-        //"host" => "localhost",
-        //"prefix" => "wp_",
-    ),
+$user_config = array(
+	'auto_installer' => false,
+	'db' => array(
+		//'name' => 'wordpress',
+		//'user' => '',
+		//'pwd' => '',
+		//'host' => 'localhost',
+		//'prefix' => 'wp_',
+	),
+	//'site_title' => 'My new site',
+	'user' => array(
+		//'name' => '',
+		//'pwd' => '',
+		//'email' => '',
+	),
+	//'blog_public' => '1',
+	//'page_on_front' => '1',
+	//'permalink_structure' => '/%postname%/',
+	//'set_avatar' => '1',
+	//'del_default' => '1',
+	//'install_theme' => '',
+	//'install_plugin' => '',
 );
 
 $file = "wp-quick-install.php";
