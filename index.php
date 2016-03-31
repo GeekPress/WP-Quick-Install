09865b9211cdc2d67fa5837aa476bd20bd374b6f
Pravdomil <pravdomil.toman@gmail.com>
2016-03-31 20:18:57 +0200

    docs

--- index.php
+++ index.php
@@ -1,17 +1,8 @@
 <?php
 /*
 
-Config property will be taken from:
-1. $user_config variable
-	if specified
-2. then from browser cookie
-	which is saved after each succesfull form submit
-	admin username, admin password, db prefix and site_title is not saved into cookie
-	cookie is set for all domain
-3. otherwise from installer defaults
-	specified in html
-
-For auto installer: set skip_welcome, submit_db, submit_config to true
+For auto installer set:
+skip_welcome, submit_db, submit_config = true
 
 */
 
